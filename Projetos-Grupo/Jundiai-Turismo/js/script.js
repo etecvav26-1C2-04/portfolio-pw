805fa1b3a145bba65288270b6f909397b573d6c4
Giovanna Cabral <giovannaccabral2@gmail.com>
2026-08-21 01:07:46 -0300

    Create script.js

---
+++ Projetos-Grupo/Jundiai-Turismo/js/script.js
@@ -0,0 +1,28 @@
+function Descubra() {
+    location.href = "curiosidades.html";
+
+}
+
+function brincar() {
+    location.href = "brincar.html";
+}
+
+function centrohistorico() {
+    location.href = "centrohistorico.html";
+}
+
+function sabores() {
+    location.href = "sabores.html";
+}
+
+function uvaVinho() {
+    location.href = "uvaevinho.html";
+}
+
+function culturaItaliana() {
+    location.href = "cultItalia.html";
+}
+
+
+var meubotao = document.querySelector('#cursor');
+meubotao.style.cursor = 'pointer';
